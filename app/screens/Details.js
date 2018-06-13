29bf61d7c2f3eb6627b964032fd5b3b2cb83844e
Quasimodo <quasimodo1660@gmail.com>
2018-06-13 10:14:47 -0700

    add background mode in info.plist

--- app/screens/Details.js
+++ app/screens/Details.js
@@ -8,7 +8,7 @@ import SocketIOClient from 'socket.io-client';
 export default class DetailsScreen extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.socket=SocketIOClient('http://13.57.221.111:6789');
+    this.socket=SocketIOClient('http:///127.0.0.1:6789');
     this.jsuser={}
   }
   static navigationOptions = ({navigation}) => {
