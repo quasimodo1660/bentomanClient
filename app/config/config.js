c78e85d576011d6cd681b11eff6abaf75848aaea
Quasimodo <quasimodo1660@gmail.com>
2018-06-23 00:02:50 -0700

    add github login

--- app/config/config.js
+++ app/config/config.js
@@ -5,6 +5,9 @@ const config ={
     'socketServer':{
         // server:'http://13.57.221.111:6789'
         server:'http://127.0.0.1:6789'
+    },
+    'githubLogin':{
+        url:'http://127.0.0.1:8000/accounts/github/login'
     }
 }
 
--- app/screens/SignIn.js
+++ app/screens/SignIn.js
@@ -1,28 +1,87 @@
 import React from 'react';
 import {
   AsyncStorage,
+  Linking,
+  Platform,
   View,
   StyleSheet,
-  Button
+  Button,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import SafariView from 'react-native-safari-view';
+import config from '../config/config'
+import axios from 'axios'
+import { toJS } from 'mobx'
 
 export default class SignInScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this._handleOpenURL = this._handleOpenURL.bind(this);
+  } 
+
+
+
     static navigationOptions = {
       title: 'Please sign in',
     };
+
+    componentDidMount() {
+      Linking.addEventListener('url', this._handleOpenURL);
+    }
+
+    componentWillUnmount() {
+      Linking.removeEventListener('url', this._handleOpenURL);
+    }
+
+
+
+    openURL = (url) =>{
+      if (Platform.OS === 'ios') {
+        SafariView.show({
+          url: url,
+          fromBottom: true,
+        });
+      }
+      // Or Linking.openURL on Android
+      else {
+        Linking.openURL(url);
+      }
+    }
+
+    loginWithGithub = () => {
+      this.openURL(config.githubLogin.url)
+    }
+
+    _handleOpenURL(event) {
+      console.log(this)
+      console.log(event.url.match(/\d+/g)[0]);
+      axios.get('http://127.0.0.1:8000/accounts/renderUser/'+event.url.match(/\d+/g)[0]).then(res=>{
+        console.log(res)
+        AsyncStorage.setItem('StoreUser', JSON.stringify(toJS(res.data)));
+        if (Platform.OS === 'ios') {
+          SafariView.dismiss();
+        }
+      })
+      this.props.navigation.navigate('App')
+    }
   
-    render() {
+  render() {
       return (
         <View style={styles.container}>
-          <Button title="Sign in!" onPress={this._signInAsync} />
+          <Icon.Button
+            name="github"
+            backgroundColor="#3b5998"
+            onPress={this.loginWithGithub}
+            // {...iconStyles}
+          >
+            Login with Github
+          </Icon.Button>
         </View>
       );
     }
   
-    _signInAsync = async () => {
-      await AsyncStorage.setItem('userToken', 'abc');
-      this.props.navigation.navigate('App');
-    };
+   
   }
 
   const styles = StyleSheet.create({
